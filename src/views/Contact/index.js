b2ac9301d1aa80e61dc7ff27fb2eb08d5006b293
Minhvan Le <mple330@gmail.com>
2022-06-17 16:27:39 -0700

    contact section complete

---
+++ src/views/Contact/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ContactForm from "../../components/ContactForm";
+import SocialLinks from "../../components/SocialLinks";
+import "./style.css";
+
+function Contact() {
+    return (
+        <div className="viewFive" id="contact">
+            <div className="row contact-row">
+                <div className="formDiv" id="contact">
+                    <h3 className="viewTitle" id="contactTitle">
+                        GET IN TOUCH
+                    </h3>
+
+                    <hr></hr>
+
+                    {/* <ContactForm /> */}
+                    <SocialLinks />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Contact;
